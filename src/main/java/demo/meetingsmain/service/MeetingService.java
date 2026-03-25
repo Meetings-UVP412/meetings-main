59518c8dcc6a6a7dafb735db0fba52ef18f4f317
Michael <michaelkivo977@gmail.com>
2026-03-25 15:55:58 +0300

    add logs to main-service

--- src/main/java/demo/meetingsmain/service/MeetingService.java
+++ src/main/java/demo/meetingsmain/service/MeetingService.java
@@ -5,6 +5,8 @@ import demo.meetingscontracts.dto.MeetingResponse;
 import demo.meetingscontracts.dto.MeetingStatus;
 import demo.meetingscontracts.exceptions.ResourceNotFoundException;
 import demo.meetingsmain.storage.InMemoryStorage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,6 +17,7 @@ import java.util.UUID;
 @Service
 public class MeetingService {
     private final InMemoryStorage storage;
+    private static final Logger log = LoggerFactory.getLogger(MeetingService.class);
 
     public MeetingService(InMemoryStorage storage) {
         this.storage = storage;
@@ -38,6 +41,8 @@ public class MeetingService {
                 MeetingStatus.NEW);
 
         storage.meetings.put(meetingResponse.uid(), meetingResponse);
+        log.info("Created new meeting: {}", meetingResponse);
+
         return meetingResponse;
     }
 
--- src/main/java/demo/meetingsmain/service/RedisService.java
+++ src/main/java/demo/meetingsmain/service/RedisService.java
@@ -5,12 +5,13 @@ import demo.meetingscontracts.dto.MeetingResponse;
 import demo.meetingscontracts.dto.MeetingStatus;
 import demo.meetingscontracts.exceptions.IllegalArgumentException;
 import demo.meetingsmain.config.RabbitMQConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Service
@@ -20,6 +21,7 @@ public class RedisService {
     private RedisTemplate<String, Object> redisTemplate;
     private final StringRedisTemplate stringRedisTemplate;
     private final RabbitTemplate rabbitTemplate;
+    private static final Logger log = LoggerFactory.getLogger(RedisService.class);
 
     private final MeetingService meetingService;
 
@@ -39,6 +41,7 @@ public class RedisService {
         }
 
         redisTemplate.opsForValue().set(fullPath, audioData); // save audio in redis
+        log.info("Saved new chunk: UUID: {} ord: {} isLast: {}", uuid, ord, isLast);
 
         // publish event: chunk downloaded
         ChunkDownloadedEvent event = new ChunkDownloadedEvent(
@@ -48,6 +51,7 @@ public class RedisService {
                 0
         );
         rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY_CHUNK_DOWNLOADED, event);
+        log.info("Sent new message ro rabbitmq about new chunk: {} ", fullPath);
     }
 
     public byte[] getAudio(String key) {
@@ -68,6 +72,8 @@ public class RedisService {
         } else {
             stringRedisTemplate.opsForValue().set(fullPath, currentValue + "\n" + result);
         }
+
+        log.info("Updated result text for meeting: {}", uuid);
     }
 
     public String getMeetingResult(UUID uuid) {
