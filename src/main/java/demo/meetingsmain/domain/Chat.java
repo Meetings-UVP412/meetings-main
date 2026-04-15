eb354ecfa3712f0a1ea9fcab05d29cb8080b5c47
Michael <michaelkivo977@gmail.com>
2026-04-15 09:11:00 +0300

    add chat entity

--- src/main/java/demo/meetingsmain/domain/Chat.java
+++ src/main/java/demo/meetingsmain/domain/Chat.java
@@ -1,4 +1,81 @@
 package demo.meetingsmain.domain;
 
-public class Chat {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "chats")
+public class Chat extends BaseEntityUUID {
+    @Column(name = "meeting_uuid")
+    private String meetingUUID;
+
+    private String title;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb")
+    private List<Message> messages = new ArrayList<>();
+
+    private LocalDateTime createdAt;
+
+    protected Chat() { }
+
+    public Chat(String meetingUUID, String title, List<Message> messages, LocalDateTime createdAt) {
+        this.meetingUUID = meetingUUID;
+        this.title = title;
+        this.messages = messages;
+        this.createdAt = createdAt;
+    }
+
+    public String getMeetingUUID() {
+        return meetingUUID;
+    }
+
+    public void setMeetingUUID(String meetingUUID) {
+        this.meetingUUID = meetingUUID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Chat{" +
+                "meetingUUID='" + meetingUUID + '\'' +
+                ", title='" + title + '\'' +
+                ", messages=" + messages +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
+
+class Message {
+    private String role;
+    private String content;
 }
