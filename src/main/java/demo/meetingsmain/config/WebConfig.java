97eb1704393e507931ce3874b85e8e6c6dd65186
Michael <michaelkivo977@gmail.com>
2026-04-16 17:19:22 +0300

    add web config

---
+++ src/main/java/demo/meetingsmain/config/WebConfig.java
@@ -0,0 +1,15 @@
+package demo.meetingsmain.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/chats/send")
+                .allowedOrigins("*")
+                .allowedMethods("POST");
+    }
+}
