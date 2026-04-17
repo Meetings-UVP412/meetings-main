227345541273fab6959df5bed823eeaa0c3f6c3d
Michael <michaelkivo977@gmail.com>
2026-04-17 11:06:58 +0300

    add to meetingResponse users dto & author FIO

--- src/main/java/demo/meetingsmain/service/impl/MeetingServiceImpl.java
+++ src/main/java/demo/meetingsmain/service/impl/MeetingServiceImpl.java
@@ -3,12 +3,14 @@ package demo.meetingsmain.service.impl;
 import demo.meetingscontracts.dto.MeetingRequest;
 import demo.meetingscontracts.dto.MeetingResponse;
 import demo.meetingscontracts.dto.MeetingStatus;
+import demo.meetingscontracts.dto.UserDTO;
 import demo.meetingscontracts.exceptions.ResourceNotFoundException;
 import demo.meetingsmain.domain.Meeting;
 import demo.meetingsmain.domain.User;
 import demo.meetingsmain.repository.MeetingRepository;
 import demo.meetingsmain.repository.UserRepository;
 import demo.meetingsmain.service.MeetingService;
+import demo.meetingsmain.service.UserService;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +24,13 @@ import java.util.stream.Collectors;
 public class MeetingServiceImpl implements MeetingService {
     private MeetingRepository meetingRepository;
     private UserRepository userRepository;
+    private UserService userService;
     private static final Logger log = LoggerFactory.getLogger(MeetingServiceImpl.class);
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @Autowired
     public void setUserRepository(UserRepository userRepository) {
@@ -69,16 +77,26 @@ public class MeetingServiceImpl implements MeetingService {
         Meeting savedMeeting = meetingRepository.save(meeting);
         log.info("Created new meeting: {}", meeting);
 
+        UserDTO authorDto = userService.findById(author.getId());
+        String authorName = authorDto.lastName() + " " +
+                authorDto.firstName().charAt(0) + "." +
+                authorDto.patronymic().charAt(0) + ".";
+
+        Set<UserDTO> participantDtos = foundUsers.stream()
+                .map(user -> userService.findById(user.getId()))
+                .collect(Collectors.toSet());
+
         return new MeetingResponse(
                 savedMeeting.getId(),
                 savedMeeting.getName(),
-                new HashSet<>(request.users()),
                 savedMeeting.getAuthor().getId(),
+                authorName,
                 savedMeeting.getCreatedAt(),
                 savedMeeting.getDuration(),
                 savedMeeting.getComment(),
                 savedMeeting.getLink(),
-                savedMeeting.getStatus()
+                savedMeeting.getStatus(),
+                participantDtos
         );
     }
 
@@ -110,20 +128,23 @@ public class MeetingServiceImpl implements MeetingService {
     }
 
     private MeetingResponse toResponse(Meeting meeting) {
-        Set<Integer> participantIds = meeting.getParticipants().stream()
-                .map(User::getId)
+        Set<UserDTO> participantDtos = meeting.getParticipants().stream()
+                .map(user -> userService.findById(user.getId()))
                 .collect(Collectors.toSet());
+        UserDTO author = userService.findById(meeting.getAuthor().getId());
+        String authorName = author.lastName() + " " + author.firstName().charAt(0) + "." + author.patronymic().charAt(0) + ".";
 
         return new MeetingResponse(
                 meeting.getId(),
                 meeting.getName(),
-                participantIds,
                 meeting.getAuthor().getId(),
+                authorName,
                 meeting.getCreatedAt(),
                 meeting.getDuration(),
                 meeting.getComment(),
                 meeting.getLink(),
-                meeting.getStatus()
-        );
+                meeting.getStatus(),
+                participantDtos
+                );
     }
 }
